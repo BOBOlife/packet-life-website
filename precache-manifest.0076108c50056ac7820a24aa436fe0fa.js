2f4699211b540ae433d2cb80393269549201e1b9
BOBOWang <bbw1996@qq.com>
2020-10-26 14:16:26 +0800

    deploy

---
+++ precache-manifest.0076108c50056ac7820a24aa436fe0fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "775f7be37521ac1a0e20",
+    "url": "/packet-life-website/css/app.98e0dc87.css"
+  },
+  {
+    "revision": "10d055de4c586723548ceabb1ed7a913",
+    "url": "/packet-life-website/index.html"
+  },
+  {
+    "revision": "775f7be37521ac1a0e20",
+    "url": "/packet-life-website/js/app.59bba785.js"
+  },
+  {
+    "revision": "29868dcc53a1c0bcedb4",
+    "url": "/packet-life-website/js/chunk-vendors.e31517cd.js"
+  },
+  {
+    "revision": "f33f1cf23fa6f780b9addb28bd87f32e",
+    "url": "/packet-life-website/manifest.json"
+  },
+  {
+    "revision": "c580781771d6d2c5ae29fe8d168c58ae",
+    "url": "/packet-life-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/packet-life-website/robots.txt"
+  }
+]);
